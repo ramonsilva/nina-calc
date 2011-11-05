6dbc888ee074bd7961586f812e611f7a59acb342
ramon <ramon@Morpheus.(none)>
2011-11-05 19:44:49 -0200

    teste

--- ok.spec.js
+++ ok.spec.js
@@ -2,5 +2,9 @@ describe("ok", function() {
 	it("expect ok be really ok", function() {
 		expect('ok').toEqual('ok');
 	});
+
+	it("expect ok be really fail", function() {
+		expect('ok').toEqual('fail');
+	});
 });	
 
